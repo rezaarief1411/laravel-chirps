365aff310458986d2a0698af674b68e13d0801e4
rezaarief1411 <reza.arief@sirclo.com>
2024-07-21 17:47:33 +0700

    laravel bootcamp

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
+use App\Http\Requests\ChirpStoreUpdateRequest;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        // return response("Hello World!",200);
+        // return view('chirps.index');
+        return view('chirps.index',[
+            'chirps'=>Chirp::with('user')->latest()->get()
+            // 'chirps'=>Chirp::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    // public function store(Request $request): RedirectResponse
+    public function store(ChirpStoreUpdateRequest $request): RedirectResponse
+    {
+        // $validated = $request->validate([
+        //     'message' => 'required|max:255|string'
+        // ]);
+        $validated = $request->validated();
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp): View
+    {
+        Gate::authorize('update',$chirp);
+        return view('chirps.edit',[
+            'chirp'=>$chirp
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ChirpStoreUpdateRequest $request, Chirp $chirp)
+    {
+        Gate::authorize('update',$chirp);
+
+        $validated = $request->validated();
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        Gate::authorize('delete',$chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+}
